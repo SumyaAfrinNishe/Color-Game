92b6dd022be05dff9de9a71525b508552af6d475
SumyaAfrinNishe <afrinnishe99@gmail.com>
2024-11-25 04:31:11 +0600

    fundamental

---
+++ main.js
@@ -0,0 +1,18 @@
+var square=document.querySelectorAll(".square");
+//console.log(square);
+
+
+var colors =[
+    'rgb(255, 0, 0)',
+    'rgb(255, 255, 0)',
+    'rgb(255, 255, 255)',
+    'rgb(0, 255, 0)',
+    'rgb(255, 0, 255)',
+    'rgb(0, 0, 255)',
+
+];
+
+for(var i=0; i < colors.length; i++)
+{
+    alert(colors[i]);
+}
