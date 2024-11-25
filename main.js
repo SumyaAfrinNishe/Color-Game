6862505cda790dd465aeba1edb0babfae939a991
SumyaAfrinNishe <afrinnishe99@gmail.com>
2024-11-26 04:22:32 +0600

    Pick Color

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var square=document.querySelectorAll(".square");
+var displayColor = document.querySelector(".displayColor");
 //console.log(square);
 
 
@@ -12,7 +13,15 @@ var colors =[
 
 ];
 
-for(var i=0; i < colors.length; i++)
+var pickedColor = colors[2];
+displayColor.textContent = pickedColor;
+for(var i=0; i < square.length; i++)
 {
-    alert(colors[i]);
+    // console.log(square[i]);
+    console.log(i);
+    square[i].style.backgroundColor = colors[i];
+    square[i].addEventListener("click", function()
+{
+//     alert("you clicked inside the box.")
+})
 }
